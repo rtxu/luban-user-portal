09d39f3e924511005dce6a681b4f099c926e360f
ruitao.xu <ruitao.xu@aliyun.com>
2019-09-05 17:33:10 +0800

    feat(TemplateEngine): impl evaluateExpression

--- src/util/TemplateEngine.js
+++ src/util/TemplateEngine.js
@@ -7,6 +7,9 @@ const TmplType = {
 // 匹配规则：任何出现在 {{ 和 }} 之间的内容
 // 注意：? 使得表达式进入 non-greedy mode，greedy mode 会将多个表达式匹配成一个，非预期效果
 const JSCodeSnippetRE = /{{(.*?)}}/g;
+// 匹配规则：a.b.c
+// \b 代表 word boundary, ref: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions/Boundaries
+const EvalNodeIdRE =/(\w+[.])*\w+\b/g;
 
 function parse(tmpl) {
   const codeSnippetRE = /{{(.*?)}}/g;
@@ -15,51 +18,48 @@ function parse(tmpl) {
   while (match = codeSnippetRE.exec(tmpl)) {
     codeSnippets.push(match[1]);
   }
+  const depsMap = {};
+  for (const code of codeSnippets) {
+    const match = code.match(EvalNodeIdRE);
+    if (match) {
+      for (const dep of match) {
+        depsMap[dep] = true;
+      }
+    }
+  }
+
+  const rtnValue = {
+    codeSnippets,
+    depsMap,
+  }
 
   if (codeSnippets.length === 0) {
     return {
+      ...rtnValue,
       type: TmplType.Literal,
-      codeSnippets,
     };
   } else if (codeSnippets.length === 1) {
     if (tmpl.trim().length == codeSnippets[0].length + '{{}}'.length) {
-      // no other string
+      // nothing outside the {{}}
       return {
+        ...rtnValue,
         type: TmplType.Expression,
-        codeSnippets,
       }
     } else {
       return {
+        ...rtnValue,
         type: TmplType.TemplateString,
-        codeSnippets,
       }
     }
   } else {
     return {
+      ...rtnValue,
       type: TmplType.TemplateString,
-      codeSnippets,
     }
   }
 }
 
-function renderExpression(tmpl, ctx) {
-  const re = /{{([^}]+)?}}/g;
-  const reExp = /(^( )?(if|for|else|switch|case|break|{|}))(.*)?/g;
-  let code = 'var r=[];\n';
-  let cursor = 0;
-  let match;
-  const add = function(line, js) {
-      js? (code += line.match(reExp) ? line + '\n' : 'r.push(' + line + ');\n') :
-          (code += line != '' ? 'r.push("' + line.replace(/"/g, '\\\\"') + '");\n' : '');
-      return add;
-  }
-  while(match = re.exec(tmpl)) {
-      add(tmpl.slice(cursor, match.index))(match[1], true);
-      cursor = match.index + match[0].length;
-  }
-  add(tmpl.substr(cursor, tmpl.length - cursor));
-  code += 'return r.join("");';
-
+function callFn(functionBody, ctx) {
   const paramKeys = [];
   const paramValues = [];
   if (ctx) {
@@ -69,14 +69,13 @@ function renderExpression(tmpl, ctx) {
     }
   }
 
-  // console.log('function param name: ', paramKeys);
-  // console.log('function param value: ', paramValues);
-  // console.log('code: \n', code);
-  const functionBody = code.replace(/[\r\t\n]/g, '');
-  // console.log('function body: ', functionBody);
   const fn = new Function(...paramKeys, functionBody);
   const result = fn.apply(null, paramValues);
   return result;
+}
+
+function evaluateExpression(expression, ctx) {
+  return callFn(`return (${expression})`, ctx);
 }
 
 /**
@@ -86,50 +85,62 @@ function renderExpression(tmpl, ctx) {
  * @param  {object} [ctx]     The context used to render template
  * @return {string}           The rendered result
  */
+function renderTemplateString_v1_deprecated(tmpl, ctx) {
+  // 思路：生成一个模板字符串，js code 的地方使用 evaluateExpression 获得结果
+  const codeSnippetRE = /{{(.*?)}}/g;
+  const TemplateStringMark = {
+    begin: '`',
+    end: '`',
+  };
+  let functionBody = `return (${TemplateStringMark.begin}`;
+  let cursor = 0;
+  let match;
+  while (match = codeSnippetRE.exec(tmpl)) {
+    functionBody += tmpl.slice(cursor, match.index);
+    functionBody += '${evaluateExpression(' + match[1] + ', ctx)}'
+    cursor = match.index + match[0].length;
+  }
+  functionBody += tmpl.substr(cursor, tmpl.length - cursor) + `${TemplateStringMark.end})`;
+  console.log('functionBody', functionBody);
+  const newCtx = {
+    ...ctx,
+    ctx,
+    evaluateExpression,
+  }
+  return callFn(functionBody, newCtx);
+}
+
+// v1 的缺点：
+// 1. 引入了额外的 ctx，ctx 和 evaluateExpression
+// 2. 多次调用 evaluateExpression，不知性能如何，未实测
 function renderTemplateString(tmpl, ctx) {
-  const re = /{{([^}]+)?}}/g;
-  const reExp = /(^( )?(if|for|else|switch|case|break|{|}))(.*)?/g;
-  let code = 'var r=[];\n';
+  const codeSnippetRE = /{{(.*?)}}/g;
+  let code = '\'use strict\'; const r=[];\n';
   let cursor = 0;
   let match;
   const add = function(line, js) {
-      js? (code += line.match(reExp) ? line + '\n' : 'r.push(' + line + ');\n') :
+      js? (code += 'r.push(' + line + ');\n') :
           (code += line != '' ? 'r.push("' + line.replace(/"/g, '\\\\"') + '");\n' : '');
       return add;
   }
-  while(match = re.exec(tmpl)) {
+  while(match = codeSnippetRE.exec(tmpl)) {
       add(tmpl.slice(cursor, match.index))(match[1], true);
       cursor = match.index + match[0].length;
   }
   add(tmpl.substr(cursor, tmpl.length - cursor));
   code += 'return r.join("");';
-
-  const paramKeys = [];
-  const paramValues = [];
-  if (ctx) {
-    for (const paramKey of Object.keys(ctx)) {
-      paramKeys.push(paramKey);
-      paramValues.push(ctx[paramKey]);
-    }
-  }
-
-  // console.log('function param name: ', paramKeys);
-  // console.log('function param value: ', paramValues);
-  // console.log('code: \n', code);
   const functionBody = code.replace(/[\r\t\n]/g, '');
-  // console.log('function body: ', functionBody);
-  const fn = new Function(...paramKeys, functionBody);
-  const result = fn.apply(null, paramValues);
-  return result;
+  // console.log('functionBody', functionBody);
+  return callFn(functionBody, ctx);
 }
 
 function render(tmpl, ctx) {
-  const { type } = parse(tmpl);
+  const { type, codeSnippets } = parse(tmpl);
   switch (type) {
     case TmplType.Literal:
       return tmpl;
     case TmplType.Expression:
-      return renderExpression(tmpl, ctx);
+      return evaluateExpression(codeSnippets[0], ctx);
     case TmplType.TemplateString:
       return renderTemplateString(tmpl, ctx);
   }
--- src/util/TemplateEngine.test.js
+++ src/util/TemplateEngine.test.js
@@ -28,6 +28,15 @@ test('parse template', () => {
   }
 })
 
+test('parse template for deps', () => {
+  {
+    let { depsMap } = TemplateEngine.parse(' {{a.b.c}} {{aa.bb.cc}} {{aaa.b.ccc}}');
+    expect(Object.keys(depsMap)).toContain('a.b.c');
+    expect(Object.keys(depsMap)).toContain('aa.bb.cc');
+    expect(Object.keys(depsMap)).toContain('aaa.b.ccc');
+  }
+})
+
 test('render without ctx', () => {
   // undefined
   expect(TemplateEngine.render('1+1 = {{1+1}}')).toBe('1+1 = 2');
@@ -45,4 +54,15 @@ test('render with ctx', () => {
     b: 1,
   }
   expect(TemplateEngine.render('a+b = {{a+b}}', ctx)).toBe('a+b = 2');
+})
+
+test('render expression', () => {
+  expect(TemplateEngine.render('{{ undefined }}')).toBeUndefined();
+  expect(TemplateEngine.render('{{ null }}')).toBeNull();
+  expect(TemplateEngine.render('{{ 5 }}')).toBe(5);
+  expect(TemplateEngine.render('{{ "str" }}')).toBe("str");
+  expect(TemplateEngine.render('{{ [1, 2, 3] }}')).toEqual([1, 2, 3]);
+  expect(TemplateEngine.render('{{ [a, b, c] }}', {a:1, b: 2, c:3})).toEqual([1, 2, 3]);
+  expect(() => TemplateEngine.render('{{ reference_error }}')).toThrow(ReferenceError);
+  expect(() => TemplateEngine.render('{{ }}')).toThrow(SyntaxError);
 })
