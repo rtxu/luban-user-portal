6bf04bef1272840c56846545bfebc6ace7318741
ruitao.xu <ruitao.xu@aliyun.com>
2019-10-17 01:03:50 +0800

    chore: test interactive behavior between js and ts

--- src/pages/playground/js_import_ts.js
+++ src/pages/playground/js_import_ts.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import TsComponent from './ts';
+import TsComponent, { hello } from './ts';
 
 export default function () {
+  hello("js could call ts function");
   return (
     <div>
       <h1> This is a <span style={{backgroundColor: 'blue'}}>js_import_ts</span> component. </h1>
--- src/pages/playground/ts.tsx
+++ src/pages/playground/ts.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
 
+/** here is a ts function, just print `name` */
+export function hello(name: string) {
+  console.log(name);
+}
+
 export default function () {
   return (
     <div style={{height: 400, width: 400, backgroundColor: 'yellow'}}>This is a ts component</div>
