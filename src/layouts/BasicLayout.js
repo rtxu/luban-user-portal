669d0af1982cd17f84b122f88cc7496fd18a0f7f
ruitao.xu <ruitao.xu@aliyun.com>
2019-10-24 16:47:32 +0800

    style: make logo transparent

--- src/layouts/BasicLayout.js
+++ src/layouts/BasicLayout.js
@@ -1,7 +1,7 @@
 // 注意这里我们除了从antd中引入了Layout布局组件，还引入了Menu菜单组件，Icon图标组件
 
 import { Component } from 'react';
-import { Layout, Menu, Icon } from 'antd';
+import { Layout, Menu } from 'antd';
 import Link from 'umi/link';
 
 import styles from './BasicLayout.less';
@@ -9,31 +9,22 @@ import { ReactComponent as Logo } from './logo.svg';
 
 const { Header, Footer, Sider, Content } = Layout;
 
-// 引入子菜单组件
-const SubMenu = Menu.SubMenu; 
-
 export default class BasicLayout extends Component {
   render() {
     return (
       <Layout>
         <Sider width={256} style={{ minHeight: '100vh' }}>
           <div className={styles.logo}>
-            <Logo viewBox='25 100 250 100' />
+            <Logo viewBox='40 115 220 70' />
           </div>
           <div style={{ height: '32px', background: 'rgba(255,255,255,.2)', margin: '16px'}}/>
           <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
             <Menu.Item key="1">
-              <Icon type="pie-chart" />
-              <span>Helloworld</span>
+              <Link to='#'>应用</Link>
             </Menu.Item>
-            <SubMenu
-              key="sub1"
-              title={<span><Icon type="dashboard" /><span>Dashboard</span></span>}
-            >
-                <Menu.Item key="2"><Link to="/dashboard/analysis">分析页</Link></Menu.Item>
-                <Menu.Item key="3"><Link to="/dashboard/monitor">监控页</Link></Menu.Item>
-                <Menu.Item key="4"><Link to="/dashboard/workplace">工作台</Link></Menu.Item>
-            </SubMenu>
+            <Menu.Item key="2">
+              <Link to='/settings'>设置</Link>
+            </Menu.Item>
           </Menu>
         </Sider>
         <Layout >
