afa509a04525ba6f59c5776dee2f9d544f6ad069
ruitao.xu <ruitao.xu@aliyun.com>
2019-11-29 14:28:51 +0800

    Revert "chore: generate gzipped static files when build"
    This reverts commit bf0012a77c4c341ab33b23b5c93aeef9a3f2ea7b.

--- .umirc.js
+++ .umirc.js
@@ -1,5 +1,3 @@
-const CompressionPlugin = require("compression-webpack-plugin");
-
 export default {
   // 「配置式路由」
   // 显式配置 routes 则禁掉「约定式路由」
@@ -62,15 +60,5 @@ export default {
 
   chainWebpack(config, { webpack }) {
     // console.dir(config.module.rules);
-    if (process.env.NODE_ENV === "production") {
-      //gzip压缩
-      config.plugin("compression-webpack-plugin").use(CompressionPlugin, [
-        {
-          test: /\.js$|\.html$|\.css$/, //匹配文件名
-          threshold: 10240, //对超过10k的数据压缩
-          deleteOriginalAssets: false //不删除源文件
-        }
-      ]);
-    }
   }
 };
